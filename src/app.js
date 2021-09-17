95626d6780445c80f5dadb8b0e082863e8c1a9b4
Norbert305 <sealsnorbert@yahoo.com>
2021-09-17 00:46:11 +0000

    flip random card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,40 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  project();
 };
+
+function project() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suits = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+
+  let randomNumbers = Math.floor(Math.random() * numbers.length);
+  let randomSuits = Math.floor(Math.random() * suits.length);
+
+  let middle = numbers[randomNumbers];
+  let suit = suits[randomSuits];
+
+  if (suit == "&hearts;" || suit == "&diams;") {
+    document.querySelector(".card").style.color = "red";
+  } else {
+    document.querySelector(".card").style.color = "black";
+  }
+
+  document.querySelector(".top-suit").innerHTML = suit;
+  document.querySelector(".number").innerHTML = middle;
+  document.querySelector(".bottom-suit").innerHTML = suit;
+}
